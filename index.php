0c8fce8ce01dfbd147a811bd84f78c0f9fb48928
Ahmed-Ysf <62026113+Ahmed-Ysf@users.noreply.github.com>
2021-06-04 16:40:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 
 <?php
 
-require("/home/USERS/ELEVES/SNIR2019/ayousfi/public_html/Envoyer_SMS/fonction_bdd_inc.php");
+require("./fonction_bdd_inc.php");
 
 position($altitude2, $longitude2, $latitude2, $diff);
+echo "alt : $altitude2\n";
+echo "diff : $diff\n";
 if ($altitude2 < 2000 && $diff > 10 ) {
-    $retour = EnvoyerSMS(NUMERO, "l'altitude : $altitude2 , la longitude : $longitude2,"
-            . " la latitude : $latitude2", CLE_API);
+    $message = formerMessageSMS("C50EE9", $longitude2, $latitude2, $altitude2);
+    $retour = EnvoyerSMS(NUMERO, $message, CLE_API);
     echo $retour;
 }
 ?>
-
